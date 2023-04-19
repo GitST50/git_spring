53a4894bc71122d59dbde12f506e77506c30f0dd
GitST50 <tlstjdxo50@gmail.com>
2023-04-20 02:05:57 +0900

    11th

--- src/main/java/com/co/kr/vo/WikiListVo.java
+++ src/main/java/com/co/kr/vo/WikiListVo.java
@@ -1,5 +1,15 @@
 package com.co.kr.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WikiListVo {
+	
+	private String wkisEdit;
+	private String wkseq;
+	private String wkkeyword;
+	private String wkcontent;
 
 }
